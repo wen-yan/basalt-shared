d0318acfce74af06d60cd0f1ec498f2d362b6260
Yan Wen <yan.wen@thetradedesk.com>
2025-12-31 14:26:15 +0800

    add icons for Basalt projects

---
+++ src/BasaltIconsGenerator/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace BasaltIconsGenerator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Generator.Generate(new RenderContext(Constants.Theme), "../../../../../icons");
+    }
+}
